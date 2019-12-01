ec05c7e1a84a704c52a728eed051d1d7748835af
Guilherme Arcencio <47733489+GuiArcencio@users.noreply.github.com>
2019-12-01 14:25:38 -0300

    Implementando SFML na main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
+
+#include <SFML/Graphics.hpp>
+
 #include "BatalhaNaval.h"
 #include "Board.h"
-#include <iostream>
+
 int main(int argc, char const *argv[])
 {
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Batalha Naval", sf::Style::Close);
+	window.setVerticalSyncEnabled(true);
+
 	int rows, cols;
 	string player;
 	cout<<"nome"<<endl;
@@ -15,18 +21,24 @@ int main(int argc, char const *argv[])
 	Board IA(rows,cols);
 	cout<<"criei"<<endl;
 
-	int aRow,aCol;
-	do{
-		cin>>aRow>>aCol;
-		jogador.hitPos(aRow,aCol);
-		cout<<"hit"<<aRow<<aCol<<endl;
-		if(jogador.checkVitoria()==true)
-			;
-		IA.hitPos(aRow,aCol);
-		cout<<"hit"<<aRow<<aCol<<endl;
+	while (window.isOpen()) {
+
+		// Manipulação de eventos
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+				case sf::Event::Closed:
+					window.close();
+					break;
+			}
+		}
+
+		// Desenhos
+		window.clear(sf::Color(201, 108, 55));
 
 
-	}while(!IA.checkVitoria());
+		window.display();
+	}
 
 	return 0;
 }
