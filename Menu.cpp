d22060db9b942c468212a8a27a492f4500af27c4
Guilherme Arcencio <47733489+GuiArcencio@users.noreply.github.com>
2019-12-12 00:51:00 -0300

    Ajeitando um pouco o menu

--- Menu.cpp
+++ Menu.cpp
@@ -4,21 +4,22 @@ Menu::Menu(sf::RenderWindow& window) : Cena(window) {
     font.loadFromFile("Fonts/arial.ttf");
 
     titulo.setFont(font);
-    titulo.setString("batalha naval kkkkk");
-    titulo.setCharacterSize(24);
+    titulo.setString("Batalha Naval");
+    titulo.setCharacterSize(50);
     titulo.setFillColor(sf::Color::Red);
+    titulo.setPosition((window.getSize().x - titulo.getGlobalBounds().width) / 2, 50);
 
     jogar.setFont(font);
-    jogar.setString("jogar");
-    jogar.setCharacterSize(24);
+    jogar.setString("Jogar");
+    jogar.setCharacterSize(40);
     jogar.setFillColor(sf::Color::Black);
-    jogar.setPosition(250, 300);
+    jogar.setPosition((window.getSize().x - jogar.getGlobalBounds().width) / 2, 300);
 }
 
 Menu::~Menu() {}
 
 void Menu::draw() {
-    this->window.clear(sf::Color(255, 255, 255));
+    this->window.clear(sf::Color(173, 216, 230));
     this->window.draw(this->titulo);
     this->window.draw(this->jogar);
 }
